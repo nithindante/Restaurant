32e1a5c87da9d0e2465b44bedca31cc4b96d5e61
nithindante <Rajkumar_nithin@yahoo.com>
2024-06-19 21:00:09 +0530

    Add contact page and stylized as well with css

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,4 +1,26 @@
 let contactPage = function () {
-    
+    let contactDiv = document.createElement('div');
+document.body.appendChild(contactDiv)
+
+let headingDiv = document.createElement('h3')
+headingDiv.textContent = 'Contact me';
+contactDiv.appendChild(headingDiv)
+contactDiv.classList.add('contactDiv')
+let infoDiv = document.createElement('div')
+infoDiv.classList.add('infoDiv')
+contactDiv.appendChild(infoDiv)
+
+let nameDiv = document.createElement('div')
+nameDiv.textContent = 'Kerala Tales'
+
+let addressDiv = document.createElement('div')
+addressDiv.textContent = "Noel Focus, 3rd Floor, Seaport - Airport Road Chittethukara, Kakkanad, Kochi, Kerala 682037"
+
+let phoneDiv = document.createElement('div')
+phoneDiv.textContent = "9995006300"
+
+infoDiv.appendChild(nameDiv)
+infoDiv.appendChild(addressDiv)
+infoDiv.appendChild(phoneDiv)
 }
 
